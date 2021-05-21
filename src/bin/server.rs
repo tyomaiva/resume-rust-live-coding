26a10bb76cd916014feea8415d2634a4fe9c1e97
Artem Ivashko <tyomaiva@gmail.com>
2021-05-21 23:28:01 +0200

    Step 3: trivial TCP server works

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,3 +1,16 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:9000").expect("Cannot listen to the port");
+    for stream_res in listener.incoming() {
+        match stream_res {
+            Ok(mut stream) => {
+                println!("New client is accepted!");
+            }
+            Err(e) => {
+                println!("Reception failed: {:?}", e);
+            }
+        }
+    }
+    println!("Finished.");
 }
