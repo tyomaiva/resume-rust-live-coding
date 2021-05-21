14964d0a0806070cf70025ea60342c1099ac5b5d
aivashko <aivashko@aivashko-ASUS-laptop.home>
2021-05-21 23:08:41 +0200

    Step 2: working TCP client

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,31 @@
+use clap::{load_yaml, App};
+use serde::Serialize;
+use std::io::prelude::*;
+use std::net::TcpStream;
+
+#[derive(Serialize)]
+struct Employee {
+    name: String,
+    position: String,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from(yaml).get_matches();
+
+    let name = matches.value_of("name").unwrap().to_string();
+    let position = matches.value_of("position").unwrap().to_string();
+
+    let destination = "127.0.0.1:9000";
+    let mut stream =
+        TcpStream::connect(destination).expect(&format!("Could not connect to {}", destination));
+    let payload = Employee {
+        name: name,
+        position: position,
+    };
+    let serialized = serde_json::to_vec(&payload).expect("Could not serialize the input data!");
+    stream
+        .write(&serialized)
+        .expect("Couldn't send the message!");
+    println!("The message is sent!");
 }
